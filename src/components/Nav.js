aae72c4b2270456c08aba77b252a01fb1382a38e
kevinjwesley <kevin.john.wesley@gmail.com>
2021-06-23 17:31:57 -0600

    created components and basic structure of react portfolio

---
+++ src/components/Nav.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const style = {};
+
+function Nav() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">About Me</a>
+          </li>
+          <li>
+            <a href="#">Portfolio</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+          <li>
+            <a href="#">Resume</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Nav;
