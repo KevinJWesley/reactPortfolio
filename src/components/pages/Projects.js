1c1057ee2793ca3b0febf5d1fc1fc3bb31613f8f
kevinjwesley <kevin.john.wesley@gmail.com>
2021-06-24 19:03:28 -0600

    created projects card (projects.js), and style sheet for project cards (projects.css)

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,15 +1,42 @@
 import React from "react";
+import "../../styles/Projects.css";
 
-// need to add template and push in four ex from github
+// need to push in four ex from github
 // use bootstrap cards??
 // screenshot of project
 // include github and deployed (heroku or git) links
 
 function Projects() {
   return (
-    <div>
-      <h1>Projects</h1>
-      <p>SO MANY GREAT PROJECTS TO BE LOOKED AT</p>
+    <div className="projectContainer">
+      <section className="card" style={{ width: 300, height: 300 }}>
+        <img className="cardImg" src="..." alt="..." />
+        <div className="cardBody">
+          <p className="cardText">Project Name</p>
+          <a href="#">Deployed Link</a>
+        </div>
+      </section>
+      <section className="card" style={{ width: 300, height: 300 }}>
+        <img className="cardImg" src="..." alt="..." />
+        <div className="cardBody">
+          <p className="cardText">Project Name</p>
+          <a href="#">Deployed Link</a>
+        </div>
+      </section>
+      <section className="card" style={{ width: 300, height: 300 }}>
+        <img className="cardImg" src="..." alt="..." />
+        <div className="cardBody">
+          <p className="cardText">Project Name</p>
+          <a href="#">Deployed Link</a>
+        </div>
+      </section>
+      <section className="card" style={{ width: 300, height: 300 }}>
+        <img className="cardImg" src="..." alt="..." />
+        <div className="cardBody">
+          <p className="cardText">Project Name</p>
+          <a href="#">Deployed Link</a>
+        </div>
+      </section>
     </div>
   );
 }
